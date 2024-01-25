bff58a465e304c01e7524e211c360d355406ac9b
PyMash <mashudahmedtalukdar@gmail.com>
2024-01-25 17:04:13 +0530

    Initial commit

---
+++ lib/middle_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MiddlePage extends StatefulWidget {
+  const MiddlePage({super.key});
+
+  @override
+  State<MiddlePage> createState() => _MiddlePageState();
+}
+
+class _MiddlePageState extends State<MiddlePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
